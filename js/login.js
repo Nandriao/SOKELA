4e16be62f592fcb6f1212731f1926c5bcb4d84f5
Albino Nandrião <108406340+Nandriao@users.noreply.github.com>
2022-10-14 00:09:33 +0200

    Mod_home_Page_e_ceriei_pagina_de_login__de_cadastro

---
+++ js/login.js
@@ -0,0 +1,7 @@
+function mostraInfo() {
+
+}
+
+function mostraInfo2() {
+
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+//function modo_de_cor() {
+   // document.body.style.backgroundColor = "black";
+  //  document.getElementsByClassName(".task").style.backgroundColor = "#787878";
+//};
