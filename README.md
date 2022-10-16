1e001f69a21a13bbf906b2bedf2b83255777b22f
Albino Nandrião <108406340+Nandriao@users.noreply.github.com>
2022-10-17 01:18:07 +0200

    Versão Melhorada do Projecto (SOKELA)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# SOKELA
+# sokela
  Este projecto visa criar um site que posibilita aos usuarios ganharem valores munitarios, realizando algumas tarefas simples!
--- js/main.js
+++ js/main.js
@@ -2,3 +2,48 @@
    // document.body.style.backgroundColor = "black";
   //  document.getElementsByClassName(".task").style.backgroundColor = "#787878";
 //};
+
+//function modo_de_cor() {
+   // document.body.style.backgroundColor = "black";
+  //  document.getElementsByClassName(".task").style.backgroundColor = "#787878";
+//};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+
+const debounce = function(func, wait, immediate) {
+  let timeout;
+
+  return function(...args) {
+      const context = this;
+      const later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if(callNow) func.apply(context, args);
+  };
+};
+
+function animeScroll() {
+  const windowTop =  window.pageYOffset + (window.innerHeight * 3 / 4 + 50);
+  target.forEach(function(element){
+    if((windowTop) > element.offsetTop){
+      element.classList.add(animationClass);
+    }else{
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+if (target.length) {
+  window.addEventListener('scroll', debounce(function() {
+    animeScroll();
+ }, 30))
+}
